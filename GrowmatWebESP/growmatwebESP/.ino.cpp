15a7a8fdece4615b906e813f91aec11a450324ff
vojtech <vojtech.sedlecky@andritz.com>
2018-02-26 17:09:21 +0100

    GROWMATWEB

--- GrowmatWebESP/growmatwebESP/.ino.cpp
+++ GrowmatWebESP/growmatwebESP/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-02-26 10:56:14
+//This file has been generated on 2018-02-26 17:03:18
 
 #include "Arduino.h"
 #include "libraries/ESP8266HTTPClient.h"
--- GrowmatWebESP/growmatwebESP/growmatwebESP.ino
+++ GrowmatWebESP/growmatwebESP/growmatwebESP.ino
@@ -18,7 +18,7 @@
 //#include <ArduinoOTA.h>
 ESP8266WebServer server(80);
 
-char* htmlHeader = "<html><head><meta name=\"viewport\" content=\"width=device-width\"><style type=\"text/css\">button {height:100px;width:100px;font-family:monospace;border-radius:5px;}</style></head><body><h1><a href=/>GROWMAT WEB</a></h1>";
+char* htmlHeader = "<html><head><title>GROWMATWEB</title><meta name=\"viewport\" content=\"width=device-width\"><style type=\"text/css\">button {height:100px;width:100px;font-family:monospace;border-radius:5px;}</style></head><body><h1><a href=/>GROWMAT WEB</a></h1>";
 char* htmlFooter = "<hr><a href=/settings>SYSTEM SETTINGS</a></body></html>";
 //const char HTTP_STYLE[] PROGMEM  = "<style>.c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} .l{background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAALVBMVEX///8EBwfBwsLw8PAzNjaCg4NTVVUjJiZDRUUUFxdiZGSho6OSk5Pg4eFydHTCjaf3AAAAZElEQVQ4je2NSw7AIAhEBamKn97/uMXEGBvozkWb9C2Zx4xzWykBhFAeYp9gkLyZE0zIMno9n4g19hmdY39scwqVkOXaxph0ZCXQcqxSpgQpONa59wkRDOL93eAXvimwlbPbwwVAegLS1HGfZAAAAABJRU5ErkJggg==\") no-repeat left center;background-size: 1em;}</style>";
 
@@ -458,7 +458,7 @@ void setup() {
 	  //digitalWrite(LED1_PIN, HIGH);
 #endif
 
-	  WiFi.softAP("GROWMAT-WEB", "GROWMAT-WEB");
+	  WiFi.softAP("GROWMATWEB", "GROWMATWEB");
 	  IPAddress myIP = WiFi.softAPIP();
 	  Serial.println("Starting AP ...");
 	  Serial.print("AP IP address: ");
@@ -479,13 +479,13 @@ void setup() {
 #endif
 
 		  Serial.println("Starting AP for reconfiguration ...");
-      	  wifiManager.startConfigPortal("GROWMAT-WEB");
+      	  wifiManager.startConfigPortal("GROWMATWEB");
       	  //AP
   	  }
   	  else {
 
   		  Serial.println("Starting AP or connecting to Wi-Fi ...");
-  		  wifiManager.autoConnect("GROWMAT-WEB");
+  		  wifiManager.autoConnect("GROWMATWEB");
 
 #ifdef LED1_PIN
   		  digitalWrite(LED1_PIN, HIGH);
